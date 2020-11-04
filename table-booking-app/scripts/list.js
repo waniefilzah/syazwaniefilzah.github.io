32658e056420383873a264988c5e4ae0bbe0168d
Syazwanie Filzah <waniefilzah@yahoo.com>
2020-11-05 07:21:57 +0800

    try

--- table-booking-app/scripts/list.js
+++ table-booking-app/scripts/list.js
@@ -27,7 +27,7 @@ function GetBookings(){
         row.insertCell(2).innerHTML=gEmail;
         row.insertCell(3).innerHTML=gPax;
         row.insertCell(4).innerHTML="";//remarks
-        row.insertCel(5).innerHTML="<button id='"+ buttonId+"'>Delete</button><br/>";
+        row.insertCel(5).innerHTML="<button id='"+ buttonId+"'class='btn btn-danger'>Delete</button><br/>";
          
          
          
